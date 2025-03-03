d94817441685dfd823687ee1a263b1e4fbcb3549
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-03 15:05:34 +0000

    SDK regeneration

--- src/truefoundry_sdk/types/token_pagination.py
+++ src/truefoundry_sdk/types/token_pagination.py
@@ -12,10 +12,9 @@ class TokenPagination(UniversalBaseModel):
     next_page_token: typing.Optional[str] = None
 
     if IS_PYDANTIC_V2:
-        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
+        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow")  # type: ignore # Pydantic v2
     else:
 
         class Config:
-            frozen = True
             smart_union = True
             extra = pydantic.Extra.allow
