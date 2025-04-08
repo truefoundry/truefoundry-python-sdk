6dcb96edc3aa42ec8d463beac2e864178fcfbd9d
Chirag Jain <jain.chirag925@gmail.com>
2025-04-09 01:06:48 +0530

    Allow fqn in args and pass through request options (#36)
    * Allow fqn in args and pass through request options
    
    * Fix incorrect conflict resolution

--- src/truefoundry_sdk/v1/_wrapped_clients.py
+++ src/truefoundry_sdk/v1/_wrapped_clients.py
@@ -2,153 +2,140 @@ from typing import Any, Optional, Protocol, TypeVar
 
 from pydantic import BaseModel
 
-from truefoundry_sdk.core.api_error import ApiError
-from truefoundry_sdk.core.pagination import AsyncPager, SyncPager
-from truefoundry_sdk.core.pydantic_utilities import parse_obj_as
+from ..errors import NotFoundError
+from ..core.pagination import AsyncPager, SyncPager
+from ..core.pydantic_utilities import parse_obj_as
 
 # Clients
-from truefoundry_sdk.v1.agent_versions.client import (
-    AgentVersionsClient,
-    AsyncAgentVersionsClient,
-)
-from truefoundry_sdk.v1.artifact_versions.client import (
-    ArtifactVersionsClient,
-    AsyncArtifactVersionsClient,
-)
-from truefoundry_sdk.v1.model_versions.client import (
-    ModelVersionsClient,
-    AsyncModelVersionsClient,
-)
-from truefoundry_sdk.v1.prompt_versions.client import (
-    PromptVersionsClient,
-    AsyncPromptVersionsClient,
-)
-from truefoundry_sdk.v1.tool_versions.client import (
-    ToolVersionsClient,
-    AsyncToolVersionsClient,
-)
-from truefoundry_sdk.v1.workspaces.client import WorkspacesClient, AsyncWorkspacesClient
+from ..core.request_options import RequestOptions
+from ..types.http_error import HttpError
+from ..v1.agent_versions.client import AgentVersionsClient, AsyncAgentVersionsClient
+from ..v1.artifact_versions.client import ArtifactVersionsClient, AsyncArtifactVersionsClient
+from ..v1.model_versions.client import ModelVersionsClient, AsyncModelVersionsClient
+from ..v1.prompt_versions.client import PromptVersionsClient, AsyncPromptVersionsClient
+from ..v1.tool_versions.client import ToolVersionsClient, AsyncToolVersionsClient
+from ..v1.workspaces.client import WorkspacesClient, AsyncWorkspacesClient
 
 # Response types
-from truefoundry_sdk.types.get_agent_version_response import GetAgentVersionResponse
-from truefoundry_sdk.types.get_artifact_version_response import (
-    GetArtifactVersionResponse,
-)
-from truefoundry_sdk.types.get_model_version_response import GetModelVersionResponse
-from truefoundry_sdk.types.get_prompt_version_response import GetPromptVersionResponse
-from truefoundry_sdk.types.get_tool_version_response import GetToolVersionResponse
-from truefoundry_sdk.types.get_workspace_response import GetWorkspaceResponse
+from ..types.get_agent_version_response import GetAgentVersionResponse
+from ..types.get_artifact_version_response import GetArtifactVersionResponse
+from ..types.get_model_version_response import GetModelVersionResponse
+from ..types.get_prompt_version_response import GetPromptVersionResponse
+from ..types.get_tool_version_response import GetToolVersionResponse
+from ..types.get_workspace_response import GetWorkspaceResponse
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class HasListMethod(Protocol[T]):
-    def list(
-        self, *, fqn: str, limit: Optional[int] = None, **kwargs: Any
-    ) -> SyncPager[T]: ...
+    def list(self, *, fqn: str, limit: Optional[int] = None, **kwargs: Any) -> SyncPager[T]:
+        ...
 
 
 class HasAsyncListMethod(Protocol[T]):
-    async def list(
-        self, *, fqn: str, limit: Optional[int] = None, **kwargs
-    ) -> AsyncPager[T]: ...
+    async def list(self, *, fqn: str, limit: Optional[int] = None, **kwargs) -> AsyncPager[T]:
+        ...
 
 
-def _get_by_fqn(client: HasListMethod[T], *, fqn: str) -> T:
+def _get_by_fqn(client: HasListMethod[T], *, fqn: str, request_options: Optional[RequestOptions] = None) -> T:
     result = None
-    for item in client.list(fqn=fqn, limit=1):
+    for item in client.list(fqn=fqn, limit=1, request_options=request_options):
         result = item
         break
     if not result:
-        raise ApiError(
-            body=f"No entity found with fqn {fqn}",
-            status_code=404,
+        raise NotFoundError(
+            body=HttpError(
+                message=f"No entity found with fqn {fqn}",
+                status_code=404,
+            )
         )
     return result
 
 
-async def _aget_by_fqn(client: HasAsyncListMethod[T], *, fqn: str) -> T:
+async def _aget_by_fqn(client: HasAsyncListMethod[T], *, fqn: str, request_options: Optional[RequestOptions] = None) -> T:
     result = None
-    pager = await client.list(fqn=fqn, limit=1)
+    pager = await client.list(fqn=fqn, limit=1, request_options=request_options)
     async for item in pager:
         result = item
         break
 
     if not result:
-        raise ApiError(
-            body=f"No entity found with fqn {fqn}",
-            status_code=404,
+        raise NotFoundError(
+            body=HttpError(
+                message=f"No entity found with fqn {fqn}",
+                status_code=404,
+            )
         )
     return result
 
 
 class WrappedAgentVersionsClient(AgentVersionsClient):
-    def get_by_fqn(self, *, fqn: str) -> GetAgentVersionResponse:
-        item = _get_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetAgentVersionResponse:
+        item = _get_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetAgentVersionResponse, {"data": item})
 
 
 class WrappedArtifactVersionsClient(ArtifactVersionsClient):
-    def get_by_fqn(self, *, fqn: str) -> GetArtifactVersionResponse:
-        item = _get_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetArtifactVersionResponse:
+        item = _get_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetArtifactVersionResponse, {"data": item})
 
 
 class WrappedModelVersionsClient(ModelVersionsClient):
-    def get_by_fqn(self, *, fqn: str) -> GetModelVersionResponse:
-        item = _get_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetModelVersionResponse:
+        item = _get_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetModelVersionResponse, {"data": item})
 
 
 class WrappedPromptVersionsClient(PromptVersionsClient):
-    def get_by_fqn(self, *, fqn: str) -> GetPromptVersionResponse:
-        item = _get_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetPromptVersionResponse:
+        item = _get_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetPromptVersionResponse, {"data": item})
 
 
 class WrappedToolVersionsClient(ToolVersionsClient):
-    def get_by_fqn(self, *, fqn: str) -> GetToolVersionResponse:
-        item = _get_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetToolVersionResponse:
+        item = _get_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetToolVersionResponse, {"data": item})
 
 
 class WrappedWorkspacesClient(WorkspacesClient):
-    def get_by_fqn(self, *, fqn: str) -> GetWorkspaceResponse:
-        item = _get_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetWorkspaceResponse:
+        item = _get_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetWorkspaceResponse, {"data": item})
 
 
 class WrappedAsyncAgentVersionsClient(AsyncAgentVersionsClient):
-    async def get_by_fqn(self, *, fqn: str) -> GetAgentVersionResponse:
-        item = await _aget_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    async def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetAgentVersionResponse:
+        item = await _aget_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetAgentVersionResponse, {"data": item})
 
 
 class WrappedAsyncArtifactVersionsClient(AsyncArtifactVersionsClient):
-    async def get_by_fqn(self, *, fqn: str) -> GetArtifactVersionResponse:
-        item = await _aget_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    async def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetArtifactVersionResponse:
+        item = await _aget_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetArtifactVersionResponse, {"data": item})
 
 
 class WrappedAsyncModelVersionsClient(AsyncModelVersionsClient):
-    async def get_by_fqn(self, *, fqn: str) -> GetModelVersionResponse:
-        item = await _aget_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    async def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetModelVersionResponse:
+        item = await _aget_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetModelVersionResponse, {"data": item})
 
 
 class WrappedAsyncPromptVersionsClient(AsyncPromptVersionsClient):
-    async def get_by_fqn(self, *, fqn: str) -> GetPromptVersionResponse:
-        item = await _aget_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    async def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetPromptVersionResponse:
+        item = await _aget_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetPromptVersionResponse, {"data": item})
 
 
 class WrappedAsyncToolVersionsClient(AsyncToolVersionsClient):
-    async def get_by_fqn(self, *, fqn: str) -> GetToolVersionResponse:
-        item = await _aget_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    async def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetToolVersionResponse:
+        item = await _aget_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetToolVersionResponse, {"data": item})
 
 
 class WrappedAsyncWorkspacesClient(AsyncWorkspacesClient):
-    async def get_by_fqn(self, *, fqn: str) -> GetWorkspaceResponse:
-        item = await _aget_by_fqn(self, fqn=fqn)  # type: ignore[arg-type,var-annotated]
+    async def get_by_fqn(self, fqn: str, *, request_options: Optional[RequestOptions] = None) -> GetWorkspaceResponse:
+        item = await _aget_by_fqn(self, fqn=fqn, request_options=request_options) # type: ignore[arg-type,var-annotated]
         return parse_obj_as(GetWorkspaceResponse, {"data": item})
