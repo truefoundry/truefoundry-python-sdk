3d7e85aa91b57554b09f5511002dba8fbf60be2b
Chirag Jain <jain.chirag925@gmail.com>
2025-10-15 19:16:59 +0530

    Fix mypy errors

--- src/truefoundry_sdk/client.py
+++ src/truefoundry_sdk/client.py
@@ -2,30 +2,7 @@ import os
 import typing
 
 import httpx
-
-from truefoundry_sdk._wrapped_clients import (
-    WrappedApplicationsClient,
-    WrappedArtifactsClient,
-    WrappedArtifactVersionsClient,
-    WrappedAsyncApplicationsClient,
-    WrappedAsyncArtifactsClient,
-    WrappedAsyncArtifactVersionsClient,
-    WrappedAsyncDataDirectoriesClient,
-    WrappedAsyncModelsClient,
-    WrappedAsyncModelVersionsClient,
-    WrappedAsyncPromptsClient,
-    WrappedAsyncPromptVersionsClient,
-    WrappedAsyncSecretGroupsClient,
-    WrappedAsyncWorkspacesClient,
-    WrappedDataDirectoriesClient,
-    WrappedModelsClient,
-    WrappedModelVersionsClient,
-    WrappedPromptsClient,
-    WrappedPromptVersionsClient,
-    WrappedSecretGroupsClient,
-    WrappedWorkspacesClient,
-)
-from truefoundry_sdk.base_client import AsyncBaseTrueFoundry, BaseTrueFoundry
+from .base_client import AsyncBaseTrueFoundry, BaseTrueFoundry
 
 
 class TrueFoundry(BaseTrueFoundry):
@@ -45,16 +22,86 @@ class TrueFoundry(BaseTrueFoundry):
             follow_redirects=follow_redirects,
             httpx_client=httpx_client,
         )
-        self.applications = WrappedApplicationsClient(client_wrapper=self._client_wrapper)
-        self.artifacts = WrappedArtifactsClient(client_wrapper=self._client_wrapper)
-        self.artifact_versions = WrappedArtifactVersionsClient(client_wrapper=self._client_wrapper)
-        self.data_directories = WrappedDataDirectoriesClient(client_wrapper=self._client_wrapper)
-        self.models = WrappedModelsClient(client_wrapper=self._client_wrapper)
-        self.model_versions = WrappedModelVersionsClient(client_wrapper=self._client_wrapper)
-        self.prompts = WrappedPromptsClient(client_wrapper=self._client_wrapper)
-        self.prompt_versions = WrappedPromptVersionsClient(client_wrapper=self._client_wrapper)
-        self.secret_groups = WrappedSecretGroupsClient(client_wrapper=self._client_wrapper)
-        self.workspaces = WrappedWorkspacesClient(client_wrapper=self._client_wrapper)
+
+    @property
+    def applications(self):
+        if self._applications is None:
+            from ._wrapped_clients import WrappedApplicationsClient  # noqa: E402
+
+            self._applications = WrappedApplicationsClient(client_wrapper=self._client_wrapper)
+        return self._applications
+
+    @property
+    def artifacts(self):
+        if self._artifacts is None:
+            from ._wrapped_clients import WrappedArtifactsClient  # noqa: E402
+
+            self._artifacts = WrappedArtifactsClient(client_wrapper=self._client_wrapper)
+        return self._artifacts
+
+    @property
+    def artifact_versions(self):
+        if self._artifact_versions is None:
+            from ._wrapped_clients import WrappedArtifactVersionsClient  # noqa: E402
+
+            self._artifact_versions = WrappedArtifactVersionsClient(client_wrapper=self._client_wrapper)
+        return self._artifact_versions
+
+    @property
+    def data_directories(self):
+        if self._data_directories is None:
+            from ._wrapped_clients import WrappedDataDirectoriesClient  # noqa: E402
+
+            self._data_directories = WrappedDataDirectoriesClient(client_wrapper=self._client_wrapper)
+        return self._data_directories
+
+    @property
+    def models(self):
+        if self._models is None:
+            from ._wrapped_clients import WrappedModelsClient  # noqa: E402
+
+            self._models = WrappedModelsClient(client_wrapper=self._client_wrapper)
+        return self._models
+
+    @property
+    def model_versions(self):
+        if self._model_versions is None:
+            from ._wrapped_clients import WrappedModelVersionsClient  # noqa: E402
+
+            self._model_versions = WrappedModelVersionsClient(client_wrapper=self._client_wrapper)
+        return self._model_versions
+
+    @property
+    def prompts(self):
+        if self._prompts is None:
+            from ._wrapped_clients import WrappedPromptsClient  # noqa: E402
+
+            self._prompts = WrappedPromptsClient(client_wrapper=self._client_wrapper)
+        return self._prompts
+
+    @property
+    def prompt_versions(self):
+        if self._prompt_versions is None:
+            from ._wrapped_clients import WrappedPromptVersionsClient  # noqa: E402
+
+            self._prompt_versions = WrappedPromptVersionsClient(client_wrapper=self._client_wrapper)
+        return self._prompt_versions
+
+    @property
+    def secret_groups(self):
+        if self._secret_groups is None:
+            from ._wrapped_clients import WrappedSecretGroupsClient  # noqa: E402
+
+            self._secret_groups = WrappedSecretGroupsClient(client_wrapper=self._client_wrapper)
+        return self._secret_groups
+
+    @property
+    def workspaces(self):
+        if self._workspaces is None:
+            from ._wrapped_clients import WrappedWorkspacesClient  # noqa: E402
+
+            self._workspaces = WrappedWorkspacesClient(client_wrapper=self._client_wrapper)
+        return self._workspaces
 
 
 class AsyncTrueFoundry(AsyncBaseTrueFoundry):
@@ -74,16 +121,86 @@ class AsyncTrueFoundry(AsyncBaseTrueFoundry):
             follow_redirects=follow_redirects,
             httpx_client=httpx_client,
         )
-        self.applications = WrappedAsyncApplicationsClient(client_wrapper=self._client_wrapper)
-        self.artifacts = WrappedAsyncArtifactsClient(client_wrapper=self._client_wrapper)
-        self.artifact_versions = WrappedAsyncArtifactVersionsClient(client_wrapper=self._client_wrapper)
-        self.data_directories = WrappedAsyncDataDirectoriesClient(client_wrapper=self._client_wrapper)
-        self.models = WrappedAsyncModelsClient(client_wrapper=self._client_wrapper)
-        self.model_versions = WrappedAsyncModelVersionsClient(client_wrapper=self._client_wrapper)
-        self.prompts = WrappedAsyncPromptsClient(client_wrapper=self._client_wrapper)
-        self.prompt_versions = WrappedAsyncPromptVersionsClient(client_wrapper=self._client_wrapper)
-        self.secret_groups = WrappedAsyncSecretGroupsClient(client_wrapper=self._client_wrapper)
-        self.workspaces = WrappedAsyncWorkspacesClient(client_wrapper=self._client_wrapper)
+
+    @property
+    def applications(self):
+        if self._applications is None:
+            from ._wrapped_clients import WrappedAsyncApplicationsClient  # noqa: E402
+
+            self._applications = WrappedAsyncApplicationsClient(client_wrapper=self._client_wrapper)
+        return self._applications
+
+    @property
+    def artifacts(self):
+        if self._artifacts is None:
+            from ._wrapped_clients import WrappedAsyncArtifactsClient  # noqa: E402
+
+            self._artifacts = WrappedAsyncArtifactsClient(client_wrapper=self._client_wrapper)
+        return self._artifacts
+
+    @property
+    def artifact_versions(self):
+        if self._artifact_versions is None:
+            from ._wrapped_clients import WrappedAsyncArtifactVersionsClient  # noqa: E402
+
+            self._artifact_versions = WrappedAsyncArtifactVersionsClient(client_wrapper=self._client_wrapper)
+        return self._artifact_versions
+
+    @property
+    def data_directories(self):
+        if self._data_directories is None:
+            from ._wrapped_clients import WrappedAsyncDataDirectoriesClient  # noqa: E402
+
+            self._data_directories = WrappedAsyncDataDirectoriesClient(client_wrapper=self._client_wrapper)
+        return self._data_directories
+
+    @property
+    def models(self):
+        if self._models is None:
+            from ._wrapped_clients import WrappedAsyncModelsClient  # noqa: E402
+
+            self._models = WrappedAsyncModelsClient(client_wrapper=self._client_wrapper)
+        return self._models
+
+    @property
+    def model_versions(self):
+        if self._model_versions is None:
+            from ._wrapped_clients import WrappedAsyncModelVersionsClient  # noqa: E402
+
+            self._model_versions = WrappedAsyncModelVersionsClient(client_wrapper=self._client_wrapper)
+        return self._model_versions
+
+    @property
+    def prompts(self):
+        if self._prompts is None:
+            from ._wrapped_clients import WrappedAsyncPromptsClient  # noqa: E402
+
+            self._prompts = WrappedAsyncPromptsClient(client_wrapper=self._client_wrapper)
+        return self._prompts
+
+    @property
+    def prompt_versions(self):
+        if self._prompt_versions is None:
+            from ._wrapped_clients import WrappedAsyncPromptVersionsClient  # noqa: E402
+
+            self._prompt_versions = WrappedAsyncPromptVersionsClient(client_wrapper=self._client_wrapper)
+        return self._prompt_versions
+
+    @property
+    def secret_groups(self):
+        if self._secret_groups is None:
+            from ._wrapped_clients import WrappedAsyncSecretGroupsClient  # noqa: E402
+
+            self._secret_groups = WrappedAsyncSecretGroupsClient(client_wrapper=self._client_wrapper)
+        return self._secret_groups
+
+    @property
+    def workspaces(self):
+        if self._workspaces is None:
+            from ._wrapped_clients import WrappedAsyncWorkspacesClient  # noqa: E402
+
+            self._workspaces = WrappedAsyncWorkspacesClient(client_wrapper=self._client_wrapper)
+        return self._workspaces
 
 
 TrueFoundry.__doc__ = BaseTrueFoundry.__doc__
